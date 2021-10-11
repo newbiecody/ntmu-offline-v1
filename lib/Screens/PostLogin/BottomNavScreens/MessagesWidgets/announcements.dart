d41b8df9a474b4157975faabc99fee8a0fe9a669
newbiecody <39001578+newbiecody@users.noreply.github.com>
2021-10-11 23:51:56 +0800

    mini-mini update

--- lib/Screens/PostLogin/BottomNavScreens/MessagesWidgets/announcements.dart
+++ lib/Screens/PostLogin/BottomNavScreens/MessagesWidgets/announcements.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ntmu/Components/functs.dart';
+import 'package:ntmu/Models/announcementHeaderInfo.dart';
 
 
 class announcements extends StatefulWidget{
@@ -13,13 +14,41 @@ class announcements extends StatefulWidget{
 }
 
 class announcementsState extends State<announcements>{
+  List<announcementHeaderInfo> announcementsList = [
+    announcementHeaderInfo(poster: 'Admin', messageText: 'Welcome to NTmU! I hope that you will enjoy your stay here', time: DateTime.parse("2021-07-20 20:18:04")),
+    announcementHeaderInfo(poster: 'Admin', messageText: 'There are some patches that had been added to ', time: DateTime.parse("2021-09-16 20:18:04"))
+  ];
 
-
+  generateAnnouncements(List announcements){
+    var announcementTiles = <Widget>[];
+    announcements.forEach((element) {
+      announcementTiles.add(
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              children: <Widget>[
+                Align(child: Text(element.poster), alignment: Alignment.centerLeft),
+                SizedBox(height: 10),
+                Align(child: Text(element.messageText), alignment: Alignment.centerLeft),
+                SizedBox(height: 10),
+                Align(child: Text(element.time.toString()), alignment: Alignment.centerRight)
+                ],
+            ),
+          )
+      );
+      announcementTiles.add(Divider(thickness: 1));
+      }
+    );
+    return announcementTiles;
+  }
 
   @override
   Widget build(BuildContext context){
     return Center(
-      child: Text('Announcements page'),
+      child: Column(
+        children: generateAnnouncements(announcementsList),
+      )
     );
   }
 }
